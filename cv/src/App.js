5f4367f36ab9833392ff254326c303c86dd26291
Filozofolcam <korayuymaz@gmail.com>
2020-08-28 23:27:22 +0300

    First steps with kermit

---
+++ cv/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='/kermit.png' className="App-logo" alt="kermit" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
