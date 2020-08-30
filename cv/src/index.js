82e23ec3ee7792ab21c2ac39947c89b754a70061
Filozofolcam <korayuymaz@gmail.com>
2020-08-30 20:09:28 +0300

    Entry page created, buttons added

--- cv/src/index.js
+++ cv/src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import * as serviceWorker from './serviceWorker';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+}));
+
+function App() {
+    return (
+        <div className="First-page">
+            <div className="Entry">
+                <h1>Koray Uymaz</h1>
+                <p class="lead">Interactive Resume</p>
+            </div>
+            <Button variant="outlined">Default</Button>
+            <Button variant="outlined" onClick={() => {alert('clicked')}}>Click me</Button>
+            <div className="Work-history">
+                <h1>Work</h1>
+            </div>
+        </div>
+    );
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
