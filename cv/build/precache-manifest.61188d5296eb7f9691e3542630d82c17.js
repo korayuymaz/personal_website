3cda7232da6aa846b0b75ffc76c077a530637cea
Filozofolcam <korayuymaz@gmail.com>
2020-11-09 20:30:54 +0300

    Commit build

---
+++ cv/build/precache-manifest.61188d5296eb7f9691e3542630d82c17.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd65a0c68515f61e071613b0add3b87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c5ec9c8d834a55f60e",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "39178a9e32810993ef81",
+    "url": "/static/css/main.6e649852.chunk.css"
+  },
+  {
+    "revision": "f8c5ec9c8d834a55f60e",
+    "url": "/static/js/2.5ba291d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5ba291d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39178a9e32810993ef81",
+    "url": "/static/js/main.eced3829.chunk.js"
+  },
+  {
+    "revision": "b63a4b8ca5166987a4a7",
+    "url": "/static/js/runtime-main.2ca0876f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c020ce85c2c61c44a57dd387c987759d",
+    "url": "/static/media/me.c020ce85.png"
+  }
+]);
